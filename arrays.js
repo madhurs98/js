4d2447328e31e9b00aa64adab9af03704a9a657f
madhurs98 <madhurshrivastava1998@gmail.com>
2022-02-02 17:26:06 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,30 @@
+//array declaration
+
+//let arr=[];
+//console.log(arr);
+
+let arr = [1,2,3,4,5];
+
+console.log(arr);
+
+let b=[1,'c',"i am a string",true];
+
+console.log(b);
+
+console.log(b[0]); //1
+console.log(b[1]); //true
+console.log(b[3]); //i am a string
+
+console.log("before : ",b[1]);
+b[1]="i am replaced";
+console.log("after : ,b[1]");
+
+b[4]="newly added";
+console.log(b);
+
+b[7]="i am excited"
+console.log(b);
+
+
+
+
