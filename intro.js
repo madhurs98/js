1763c4516a9541efbc7b3d33781808fec52d7817
madhurs98 <madhurshrivastava1998@gmail.com>
2022-02-02 13:20:22 +0530

    js_intro

---
+++ intro.js
@@ -0,0 +1,18 @@
+//print hello world
+
+console.log("hello pepcoders");
+
+let name="madhur";
+console.log(name)
+
+let num="1"
+let ch="a"
+let bool = true
+
+console.log(num)
+console.log(ch)
+console.log(bool)
+
+for (let i=0;i<10;i++){
+    console.log(i)
+}
