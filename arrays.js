1f081eea701c53e29547e86d4ac6d497f78684d5
madhurs98 <madhurshrivastava1998@gmail.com>
2022-02-02 19:45:42 +0530

    arrays

--- arrays.js
+++ arrays.js
@@ -3,27 +3,58 @@
 //let arr=[];
 //console.log(arr);
 
-let arr = [1,2,3,4,5];
+//let arr = [1,2,3,4,5];
 
-console.log(arr);
+//console.log(arr);
 
-let b=[1,'c',"i am a string",true];
+//let b=[1,'c',"i am a string",true];
 
-console.log(b);
+//console.log(b);
 
-console.log(b[0]); //1
-console.log(b[1]); //true
-console.log(b[3]); //i am a string
+//console.log(b[0]); //1
+//console.log(b[1]); //true
+//console.log(b[3]); //i am a string
 
-console.log("before : ",b[1]);
-b[1]="i am replaced";
-console.log("after : ,b[1]");
+//console.log("before : ",b[1]);
+//b[1]="i am replaced";
+//console.log("after : ,b[1]");
 
-b[4]="newly added";
-console.log(b);
+//[4]="newly added";
+//onsole.log(b);
 
-b[7]="i am excited"
-console.log(b);
+//[7]="i am excited"
+//onsole.log(b);//
+
+//arrays methods
+//push method
+let arr = [1,2,3,4,5]
+console.log("Before push: ",arr)
+arr.push(6)
+console.log("after push: ",arr)
+//pop method
+console.log("before pop: ",arr)
+arr.pop()
+console.log("after pop: ",arr)
+//shift method
+console.log("before shift: ",arr)
+arr.shift()
+console.log("after shift: ",arr)
+//unshift method
+console.log("before unshift:",arr)
+arr.unshift(5)
+console.log("after unshift : ",arr)
+
+//length of array
+console.log(arr.length);
+
+//matrix
+let matrix=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
+console.log(matrix);
+
 
 
 
