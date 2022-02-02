7ea166c7534f557ae28a35446a1cc66f3c164906
madhurs98 <madhurshrivastava1998@gmail.com>
2022-02-02 14:57:34 +0530

    isprime

--- intro.js
+++ intro.js
@@ -1,18 +1,41 @@
 //print hello world
 
-console.log("hello pepcoders");
+//console.log("hello pepcoders");
 
-let name="madhur";
-console.log(name)
+//let name="madhur";
+//console.log(name)
 
-let num="1"
-let ch="a"
-let bool = true
+//let n="1"
+//let ch="a"
+//let bool = true
 
-console.log(num)
-console.log(ch)
-console.log(bool)
+//console.log(num)
+//console.log(ch)
+//console.log(bool)
 
-for (let i=0;i<10;i++){
-    console.log(i)
+//for (let i=0;i<10;i++){
+    //console.log(i)
+
+//
+
+let num = 10;
+
+while(num<20){
+    console.log(num);
+    num++;
 }
+//q is prime
+let n = 233
+let isprime= true;
+for(let i=2;i*i<=n;i++){
+    if (n%i==0){
+        isprime=false;
+    }
+}
+if(isprime==true){
+    console.log("it is a prime number")
+}
+else{
+    console.log("not a prime number")
+}
+
