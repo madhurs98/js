37fec6a8e0e5b24042e774d953872d8ea977a940
madhurs98 <madhurshrivastava1998@gmail.com>
2022-02-02 16:22:51 +0530

    function all types

--- function.js
+++ function.js
@@ -11,4 +11,32 @@ function add(a,b){
 }
 add(2,3)
 
+function mul(a,b){
+    return a*b;
+}
+let product = mul(2,3);
+console.log(product)
+
+//func in first citizen in js
+
+let a = function sub(x,y){
+    return x-y;
+}
+console.log(a(10,2));
+
+//iife
+
+(function(){
+    console.log("i am iife");
+})();
+
+//iifee with parameter
+
+(function(a,b){
+    console.log(a/b);
+})(10,2);
+
+
+
+
 
