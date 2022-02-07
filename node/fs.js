ac2043390fbac1f6bb4ec755cceb16ac9307d61f
madhurs98 <madhurshrivastava1998@gmail.com>
2022-02-07 17:11:37 +0530

    fs directory

--- node/fs.js
+++ node/fs.js
@@ -1,3 +1,4 @@
+//file system
 let fs = require("fs");
 const { fileURLToPath } = require("url");
 //console.log(fs)
@@ -18,3 +19,25 @@ fs.appendFileSync('file.txt', "how are you");
 
 // delete a file
 fs.unlinkSync('file2.txt');
+
+// read a directory
+let folderContent = fs.readdirSync (__dirname);
+console.log(folderContent);
+
+//create a directory
+
+//fs.mkdirSync('new_module');
+
+//delete
+//fs.rmdirSync('new_module')
+ // check if exists or not 
+let isExists = fs.existsSync('module2');
+console.log(isExists);
+
+//copy a filoe 
+//fs.copyfileSync(path of file to be copied,path of destination
+let dest = \Users\madhurshrivastava\OneDrive\Desktop\jp dev\module2\js
+let fileName = 'file.txt'
+let destPath = path.join(dest,fileName);
+
+fs.copyFileSync('C:\Users\madhur shrivastava\OneDrive\Desktop\jp dev\module2\node\file.txt',C:\Users\madhur shrivastava\OneDrive\Desktop\jp dev\module2\js)
